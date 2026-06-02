c4f8dd7c533929d8430d33172bf253b36be9f06a
Enzo2345-create <enzoviniciusdacruzchaves20@gmail.com>
2026-06-02 16:29:57 -0300

    asdasda

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MFM - Memória do Futebol Mundial</title>
     <link rel="stylesheet" href="/PROJETO PSW/assets/css/style.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Changa+One:ital@0;1&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
 
