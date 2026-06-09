ec1144e5be5f58f82b1cd6fda5b15073864d12d2
Enzo2345-create <enzoviniciusdacruzchaves20@gmail.com>
2026-06-09 15:17:26 -0300

    fUINHA

--- includes/header.php
+++ includes/header.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MFM - Memória do Futebol Mundial</title>
-    <link rel="stylesheet" href="/PROJETO PSW/assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Changa+One:ital@0;1&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <script src="/PROJETO PSW/assets/js/fotos.js"></script>
+    <script src="assets/js/fotos.js"></script>
 </head>
 <body>
 
@@ -18,19 +18,19 @@
     <div class="logo">MFM</div>
 
     <nav>
-        <a href="/PROJETO PSW/index.php">Home</a>
-        <a href="/PROJETO PSW/selecoes.php">Seleções</a>
-        <a href="/PROJETO PSW/dreamteam.php">Dream Team</a>
-        <a href="/PROJETO PSW/votacao.php">Votação</a>
+        <a href="index.php">Home</a>
+        <a href="selecoes.php">Seleções</a>
+        <a href="dreamteam.php">Dream Team</a>
+        <a href="votacao.php">Votação</a>
     </nav>
 
     <div class="header-perfil">
         <?php if(isset($_SESSION['usuario'])): ?>
-            <a href="/PROJETO PSW/painel.php" class="avatar">
+            <a href="painel.php" class="avatar">
                 <?= strtoupper(substr($_SESSION['usuario'], 0, 2)) ?>
             </a>
         <?php else: ?>
-            <a href="/PROJETO PSW/login.php" class="btn-login">Entrar</a>
+            <a href="login.php" class="btn-login">Entrar</a>
         <?php endif; ?>
     </div>
 
